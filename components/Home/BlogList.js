f949a1162d3fbaa3742948d53f6f909557162336
Gurami Kutivadze <dealense@gmail.com>
2022-05-29 20:06:31 +0400

    main page section is back

--- components/Home/BlogList.js
+++ components/Home/BlogList.js
@@ -47,6 +47,44 @@ const BlogList = () => {
 
   return (
     <div>
+      <div className="flex justify-center">
+        <div className="my-3">
+          <div className="text-center mb-1">
+            <h2 className="font-bolder text-[32px] text-gray-800">
+              Last Fraud Activity
+            </h2>
+          </div>
+          <div>
+            <ul className="font-medium text-sm my-2 flex items-center gap-3">
+              <li className="active-tab">
+                <span className="text-[#1565D8] bg-[#E8F0FB] p-1.5 px-4 rounded-md cursor-pointer">
+                  All
+                </span>
+              </li>
+              <li>
+                <span className="text-[#1565D8] bg-[#E8F0FB] p-1.5 px-4 rounded-md cursor-pointer">
+                  Scams
+                </span>
+              </li>
+              <li>
+                <span className="text-[#1565D8] bg-[#E8F0FB] p-1.5 px-4 rounded-md cursor-pointer">
+                  Phising
+                </span>
+              </li>
+              <li>
+                <span className="text-[#1565D8] bg-[#E8F0FB] p-1.5 px-4 rounded-md cursor-pointer">
+                  Internet Banking Fraud
+                </span>
+              </li>
+              <li>
+                <span className="text-[#1565D8] bg-[#E8F0FB] p-1.5 px-4 rounded-md cursor-pointer">
+                  Identity Theft
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div className="container px-4 mx-auto my-3">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-11">
           {blogs.map((blog) => (
