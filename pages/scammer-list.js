3ba0c14ce844566c65240992065fd91a112a4f19
Basar Ozcan <basar.ozcan@virail.com>
2022-05-31 00:34:15 +0300

    fix padding of scammer list

--- pages/scammer-list.js
+++ pages/scammer-list.js
@@ -3,7 +3,7 @@ import List from "../components/ScammerList/List";
 const scammerList = () => {
   return (
     <div>
-      <div className="container px-4 mx-auto my-3">
+      <div className="container px-4 mx-auto my-12">
         <List />
       </div>
     </div>
