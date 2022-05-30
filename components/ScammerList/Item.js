34121d01a4c66fd22959a976212c6b6a6e37ca4e
Basar Ozcan <basar.ozcan@virail.com>
2022-05-31 02:06:40 +0300

    fix report scam button on mobile table

--- components/ScammerList/Item.js
+++ components/ScammerList/Item.js
@@ -12,7 +12,7 @@ const Item = ({ item }) => {
         </td>
         <td className="px-6 py-4 text-right">
           <a href="#"
-             className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5 mr-2 mb-2 focus:outline-none">
+             className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5 mr-2 mb-2 focus:outline-none whitespace-nowrap">
             Report Scam
           </a>
         </td>
